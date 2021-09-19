9bd874161bef35f1953e5ef7a0eefba79d20bbbd
Sibel <glrsibel18@gmail.com>
2021-09-20 00:00:13 +0300

    sibel second commit.

--- f3/black-dashboard-react-master/src/redux/actions/actionTypes.js
+++
@@ -1,6 +0,0 @@
-//magic stringden kurtulmak için const atıyoruz
-export const GET_BLOGS_SUCCESS = "GET_BLOG_SUCCESS"
-export const CHANGE_BLOG = "CHANGE_BLOG" //blogu değiştirme blog select ettiğimizde hangi blog seçmişiz
-export const GET_ETIKET_SUCCESS = "GET_ETIKET_SUCCESS"
-export const GET_KATEGORI_SUCCESS = "GET_KATEGORI_SUCCESS"
-export const GET_YORUM_SUCCESS = "GET_YORUM_SUCCESS"
--- f3/black-dashboard-react-master/src/redux/reducers/initialState.js
+++
@@ -1,6 +0,0 @@
-/*başlangıç statelerini burada tutuyoruz*/
-/*stateleri buradan yönetmiş oluyoruz merkezii*/
-export default{
-    currentBlog:{},
-    blogs:[]
-}
